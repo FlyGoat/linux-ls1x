bbc9b53bd2e73177dbaa20353f4d98da3fb18504
Jiaxun Yang <jiaxun.yang@flygoat.com>
2019-01-25 21:15:39 +0800

    dt-bindings: clock: Add loongson-1 clock bindings
    Loongson-1 is a series of MIPS MCUs.
    This patch add the clock bindings for loongson-1b and
    loongson-1c clock subsystem.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

---
+++ include/dt-bindings/clock/ls1c-clock.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier:	GPL-2.0
+/*
+ * Copyright (C) 2019 Jiaxun Yang <jiaxun.yang@flygoat.com>
+ *
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_LS1C_CLOCK_H__
+#define __DT_BINDINGS_CLOCK_LS1C_CLOCK_H__
+
+#define LS1C_CLK_PLL 0
+#define LS1C_CLK_CPU 1
+#define LS1C_CLK_DC 2
+#define LS1C_CLK_DDR 3
+#define LS1C_CLK_AHB 4
+#define LS1C_CLK_APB 5
+
+#endif /* __DT_BINDINGS_CLOCK_LS1C_CLOCK_H__ */
